cf03181e4de974cca1f510900d49a759b9651796
Alex Mirgorodsky <webster6667g@gmail.com>
2023-09-07 11:11:30 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './app.css'
 import LongPulling from "./LongPulling";
-import EventSourcing from "./EventSourcing";
-import WebSock from "./WebSock";
 
 function App() {
 
   return (
       <div>
-        <WebSock/>
+        <LongPulling/>
       </div>
   )
 }
